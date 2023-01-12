1a7d51a301c09c63a713f1fe1968f182f687f70e
Jevona Magdalena <jmagdalena@ictual.com>
2023-01-12 11:59:38 +0100

    Domain part implemented

--- stock/src/main/java/nl/hu/inno/thuusbezorgd/stock/StockApplication.java
+++ stock/src/main/java/nl/hu/inno/thuusbezorgd/stock/StockApplication.java
@@ -1,4 +1,4 @@
-package nl.hu.inno.stock;
+package nl.hu.inno.thuusbezorgd.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
