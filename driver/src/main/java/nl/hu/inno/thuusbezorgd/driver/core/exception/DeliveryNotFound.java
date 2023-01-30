0aa7ffda7eb8d200598ae29fc713d2efbba148b0
Jevona Magdalena <jmagdalena@ictual.com>
2023-01-30 20:11:37 +0100

    reformat

--- driver/src/main/java/nl/hu/inno/thuusbezorgd/driver/core/exception/DeliveryNotFound.java
+++ driver/src/main/java/nl/hu/inno/thuusbezorgd/driver/core/exception/DeliveryNotFound.java
@@ -1,8 +1,8 @@
 package nl.hu.inno.thuusbezorgd.driver.core.exception;
 
-public class DeliveryNotFound extends RuntimeException{
+public class DeliveryNotFound extends RuntimeException {
 
-    public DeliveryNotFound(Long deliveryId){
+    public DeliveryNotFound(Long deliveryId) {
         super(String.format("Delivery %s is invalid", deliveryId));
     }
 
