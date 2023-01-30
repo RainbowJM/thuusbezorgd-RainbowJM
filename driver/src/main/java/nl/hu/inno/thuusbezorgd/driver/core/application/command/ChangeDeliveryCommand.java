c216da6eed486fb9eb0bd16ef69a1f2342a75df7
Jevona Magdalena <jmagdalena@ictual.com>
2023-01-30 17:00:53 +0100

    deleted not needed info

--- driver/src/main/java/nl/hu/inno/thuusbezorgd/driver/core/application/command/ChangeDeliveryCommand.java
+++ driver/src/main/java/nl/hu/inno/thuusbezorgd/driver/core/application/command/ChangeDeliveryCommand.java
@@ -1,7 +1,5 @@
 package nl.hu.inno.thuusbezorgd.driver.core.application.command;
 
-import java.util.List;
-
 public record ChangeDeliveryCommand(Long deliveryId,
                                     Long orderId,
                                     String riderName) {
